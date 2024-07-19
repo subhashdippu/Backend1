56f253fb9aced4e510fde386133d6fbd3e72c20a
subhashprasad2001@gmail.com <96110162+subhashdippu@users.noreply.github.com>
2024-07-19 16:10:14 +0530

    get the collection data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
-const port = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 
 require('dotenv').config()
 
@@ -9,13 +9,6 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 
-app.get("/", (req, res) => {
-    res.send("Hello world")
-})
-
-app.listen(port, () => {
-    console.log(`App started at ${port}`)
-})
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.abawfgw.mongodb.net/?appName=Cluster0`;
@@ -27,14 +20,35 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
+tCollection = client.db("foodappdb").collection("cartItems")
+
 
 async function run() {
     try {
         await client.connect();
+
+        const menuCollections = client.db("foodappdb").collection("menus")
+        const cartCollections = client.db("foodappdb").collection("cartItems")
+
+        app.get("/menu", async (req, res) => {
+            const result = await menuCollections.find().toArray();
+            res.send(result)
+        });
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
+
+
+app.get("/", (req, res) => {
+    res.send("Hello world")
+})
+
+// console.log("register log", app._router.stack)
+app.listen(PORT, () => {
+    console.log(`App started at ${PORT}`)
+})
