c48b6db260e0fe7f271c148cf71b397c8840b6e6
subhashprasad2001@gmail.com <96110162+subhashdippu@users.noreply.github.com>
2024-07-19 15:21:34 +0530

    connecting mongodb

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express()
 const cors = require('cors');
 const port = process.env.PORT || 3000
 
+require('dotenv').config()
+
 // middleware
 app.use(cors())
 app.use(express.json())
@@ -14,3 +16,25 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
     console.log(`App started at ${port}`)
 })
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.abawfgw.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        await client.close();
+    }
+}
+run().catch(console.dir);
