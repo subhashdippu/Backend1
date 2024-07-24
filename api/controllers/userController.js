197ae22ec211908ddeff8122b36f74f13e567425
subhashprasad2001@gmail.com <96110162+subhashdippu@users.noreply.github.com>
2024-07-24 10:20:20 +0530

    creating user

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -3,12 +3,31 @@ const User = require("../model/User")
 const getAllUsers = async(req, res)=>{
     try {
         const users = await User.find({})
-        res.status(200).json(user)
+        res.status(200).json(users)
     }
     catch(error){
         res.status(500).json({message : error.message});
     }
 } 
+
+// create user
+const createUser = async (req, res) => {
+    const user = req.body;
+    const query = { email: user.email };
+    try {
+        const existingUser = await User.findOne( query );
+        if (existingUser) {
+            return res.status(302).json({message: "User is already exists!"})
+        }
+        const result = await User.create(user);
+        res.status(200).json(result)
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
 module.exports={
-    getAllUsers
+    getAllUsers, 
+    createUser,
 }
--- api/router/userRoutes.js
+++ api/router/userRoutes.js
@@ -4,5 +4,6 @@ const router = express.Router()
 
 const userController = require("../controllers/userController")
 router.get('/',userController.getAllUsers)
+router.post('/',userController.createUser)
 
 module.exports = router
