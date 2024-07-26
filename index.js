87e19090ec12d51a30b77e9f89b9711f94674d94
subhashprasad2001@gmail.com <96110162+subhashdippu@users.noreply.github.com>
2024-07-26 11:17:38 +0530

    generating token

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
-const express = require('express')
-const app = express()
-const cors = require('cors');
-const PORT = process.env.PORT || 3000
-const mongoose = require('mongoose')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = process.env.PORT || 3000;
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 // middleware
-app.use(cors())
-app.use(express.json())
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.abawfgw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
-    .then(
-        console.log("mongodb is connected")
-    )
-    .catch((error) => console.log("this is error", error))
-
+app.use(cors());
+app.use(express.json());
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.abawfgw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+  )
+  .then(console.log("mongodb is connected"))
+  .catch((error) => console.log("this is error", error));
 
 app.get("/", (req, res) => {
-    res.send("Hello world")
-})
+  res.send("Hello world");
+});
 
-const userRoutes = require("./api/router/userRoutes")
-app.use('/users',userRoutes)
+// console.log(process.env.ACCESS_TOKEN_SECRET);
+
+// jwt token generation
+app.post("/jwt", async (req, res) => {
+  const user = req.body;
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "1hr",
+  });
+  res.send({ token });
+});
+const userRoutes = require("./api/router/userRoutes");
+app.use("/users", userRoutes);
 
 // console.log("register log", app._router.stack)
 app.listen(PORT, () => {
-    console.log(`App started at ${PORT}`)
-})
+  console.log(`App started at ${PORT}`);
+});
